86f4675c1a3f5d1bf2263e7018646267ea31a49f
Dan Gerchcovich <dan.developer789@gmail.com>
2023-11-05 07:17:53 +1100

    Folder Restructure

--- retrofire.sh
+++ retrofire.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 $PODS_ROOT/SourceryForRetrofire/bin/sourcery \
     --sources $SRCROOT \
-    --templates $PODS_ROOT/RetrofireSwift/Templates/RetrofireSourcery.stencil \
+    --templates $PODS_ROOT/RetrofireSwiftV5/Templates/RetrofireSourcery.stencil \
     --output $SRCROOT/Generated/
